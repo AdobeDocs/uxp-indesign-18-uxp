81cfbb80c658c92c15cac1fb2b24464568611163
Padma Krishnamoorthy <pakrishn@Padmas-MBP.attlocal.net>
2023-03-14 13:32:18 -0700

    Fixing DOM API URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
           },
           {
             title: 'InDesign API',
-            path: '/indesign/dom/',
+            path: '/indesign/dom/api/',
           },
           {
             title: 'Recipes',
